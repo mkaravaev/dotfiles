79f9af32cc7ebf3bf89bc8c7c3fd4a48a1dec9fe
Mikhail Karavaev <mkaravaev@gmail.com>
2023-11-21 11:39:30 +0100

    Improve terminal style; add direnv hook for fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -5,10 +5,12 @@ end
 set -gx ERL_AFLAGS "-kernel shell_history enabled"
 set -gx VISUAL "opt/homebrew/bin/nvim"
 set -gx EDITOR "opt/homebrew/bin/nvim"
+set -gx HOMEBREW_NO_AUTO_UPDATE "1"
 
 # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
 complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
-
+# Hook direnv
+direnv hook fish | source
 
